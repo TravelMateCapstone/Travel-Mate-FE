a4bb599e2b2721eddd70f430b0333cf87037983d
Nhơn Trần <nhonvn2509@gmail.com>
2024-10-16 06:50:38 +0700

    Refactor Navbar component to use Bootstrap Navbar and add navigation links

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React from 'react';
+import { Navbar as BootstrapNavbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import RoutePath from '../../routes/RoutePath'
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <BootstrapNavbar bg="light" expand="lg" className="fixed-top">
+      <BootstrapNavbar.Brand href="#home">Brand</BootstrapNavbar.Brand>
+      <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
+      <BootstrapNavbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link >
+            <Link to={RoutePath.ADMIN}>Trang chủ</Link>
+          </Nav.Link>
+          <Nav.Link >
+            <Link to={RoutePath.EVENT}>Sự kiện</Link>
+          </Nav.Link>
+          <Nav.Link >
+            <Link to={RoutePath.GROUP}>Nhóm</Link>
+          </Nav.Link>
+        </Nav>
+      </BootstrapNavbar.Collapse>
+    </BootstrapNavbar>
+  );
 }
 
-export default Navbar
+export default Navbar;
