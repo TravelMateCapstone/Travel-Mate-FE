d606352a5824eb90761fb1735a20860a2ca74f32
Nhơn Trần <nhonvn2509@gmail.com>
2024-10-30 10:44:55 +0700

    Add DestinationCard, FriendsList, TravelGoals, WeatherWidget components and integrate into Home page; update package.json and package-lock.json for react-multi-carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,49 +1,29 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import SearchBar from '../components/Shared/SearchBar';
-import { Container } from 'react-bootstrap';
+import React from 'react';
+import { Container, Row, Col, Card, ProgressBar } from 'react-bootstrap';
 import ToolBar from '../components/Shared/ToolBar';
-import SidebarList from '../components/Shared/SidebarList';
-import FormSubmit from '../components/Shared/FormSubmit';
-import { useSelector } from 'react-redux';
 
 function Home() {
-  const sidebarItems = [
-    { iconName: 'home', title: 'Trang Chủ', route: '/' },
-    { iconName: 'add-circle', title: 'Thêm', route: '/add' },
-    { iconName: 'people', title: 'Người Dùng', route: '/people' },
-    { iconName: 'calendar', title: 'Lịch', route: '/calendar' },
-    { iconName: 'car', title: 'Xe Hơi', route: '/car' },
-    { iconName: 'wallet', title: 'Ví', route: '/wallet' }
-  ];
-
-  const handleFormSubmit = () => {
-    alert("Đã lưu event");
-  };
-
-  // Lấy thông tin người dùng từ Redux store
-  const user = useSelector((state) => state.auth.user);
-
-  useEffect(() => {
-    // Gọi API và in ra console
-    axios.get('https://travelmateapp.azurewebsites.net/api/groups?pageNumber=1')
-      .then(response => {
-        console.log("API Response:", response.data);
-      })
-      .catch(error => {
-        console.error("API Error:", error);
-      });
-  }, []);
-
   return (
     <div>
-      {/* Hiển thị thông tin người dùng nếu đã đăng nhập */}
-      {user ? (
-        <h2>Chào mừng {user["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"]}!</h2>
-      ) : (
-        <h2>Chào mừng khách!</h2>
-      )}
       <ToolBar />
+      <Container fluid className='mt-4' style={{
+        padding: '0 85px'
+      }}>
+        <Row>
+          {/* Left Sidebar */}
+          <Col md={2} className="sidebar">
+            sidebar
+          </Col>
+          {/* Main Content */}
+          <Col md={8}>
+            body
+          </Col>
+          {/* Right Sidebar */}
+          <Col md={2}>
+            right
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
