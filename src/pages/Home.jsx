bbefdb83a5c583cd9888142c2502f14c5dba8154
Nhơn Trần <nhonvn2509@gmail.com>
2024-10-31 16:44:47 +0700

    base home ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,43 @@
 import React from 'react';
-import { Container, Row, Col, Card, ProgressBar } from 'react-bootstrap';
 import ToolBar from '../components/Shared/ToolBar';
+import sunny from '../assets/images/sunny.png'
 
 function Home() {
   return (
     <div>
       <ToolBar />
-      <Container fluid className='mt-4' style={{
+
+      <div className='d-flex justify-content-between gap-3 flex-wrap mt-3' style={{
         padding: '0 85px'
       }}>
-        <Row>
-          {/* Left Sidebar */}
-          <Col md={2} className="sidebar">
-            sidebar
-          </Col>
-          {/* Main Content */}
-          <Col md={8}>
-            body
-          </Col>
-          {/* Right Sidebar */}
-          <Col md={2}>
-            right
-          </Col>
-        </Row>
-      </Container>
+        <div style={{
+          flex: 1
+        }}>
+          
+          <div className='weather-wildet bg-success text-white p-2'>
+            <h4 className='text-center'>Hồ Chí Minh</h4>
+           <div className='d-flex align-items-center'>
+              <div>
+                <p className='text-nowrap'><ion-icon name="cloud-outline"></ion-icon> 13km/h</p>
+                <p><ion-icon name="water-outline"></ion-icon> 20%</p>
+              </div>
+              <img src={sunny} alt="sunny" width={128}/>
+              <div>
+                <p><ion-icon name="thermometer-outline"></ion-icon> 30<sup>o</sup>C</p>
+                <p className='text-nowrap'>Nắng ấm</p>
+              </div>
+           </div>
+          </div>
+
+        </div>
+        <div style={{
+          flex: 3
+        }}>Body</div>
+        <div style={{
+          flex: 1
+        }}>Right</div>
+      </div>
+
     </div>
   );
 }
