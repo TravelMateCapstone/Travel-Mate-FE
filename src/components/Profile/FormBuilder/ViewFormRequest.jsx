dd7767cd0b33ccc3f843120cabc65399d5f276cf
Nhơn Trần <nhonvn2509@gmail.com>
2024-11-23 23:41:42 +0700

    feat: refactor fetchRequest to use async/await and handle errors for improved data fetching

--- src/components/Profile/FormBuilder/ViewFormRequest.jsx
+++ src/components/Profile/FormBuilder/ViewFormRequest.jsx
@@ -5,20 +5,22 @@ import { useSelector } from 'react-redux'
 function ViewFormRequest() {
   const token = useSelector(state => state.auth.token);
   const [requests, setRequests] = useState([]);
-  const fetchRequest = () => {
-    const response = axios.get('https://travelmateapp.azurewebsites.net/api/ExtraFormDetails/Request', {
-      headers: {
-        Authorization: `${token}`
-      }
-    })
-    setRequests(response.data)
-    console.log(response);
+  const fetchRequest = async () => {
+    try {
+      const response = await axios.get('https://travelmateapp.azurewebsites.net/api/ExtraFormDetails/Request', {
+        headers: {
+          Authorization: `${token}`
+        }
+      });
+      setRequests(response.data.$values);
+      console.log(response.data.$values);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
   }
   useEffect(() => {
     fetchRequest();
   }, [token])
-
-
   return (
     <div>ViewFormRequest</div>
   )
