0c9fd54623a99e0b7545ea80bf34d804fda742e6
Nhơn Trần <nhonvn2509@gmail.com>
2024-12-03 17:24:42 +0700

    Enhance TourList component layout and styling, add currency formatting for price

--- src/components/Profile/TourList.jsx
+++ src/components/Profile/TourList.jsx
@@ -17,21 +17,41 @@ function TourList() {
         }
     };
 
+    const formatCurrency = (price) => {
+        return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(price);
+    };
+
     return (
         <div>
             <h1>Tour List</h1>
             <ul style={{ listStyleType: 'none', padding: 0 }}>
                 {tourdata.map(tour => (
-                    <li key={tour.tourId} style={{ display: 'flex', marginBottom: '20px', border: '1px solid #ccc', padding: '10px', borderRadius: '8px' }}>
-                        <img src={tour.tourImage} alt={tour.tourName} width="200" style={{ marginRight: '20px' }} />
-                        <div>
-                            <h2>{tour.tourName}</h2>
-                            <p>Location: {tour.location}</p>
-                            <p>Price: {tour.price}</p>
-                            <p>Start Date: {new Date(tour.startDate).toLocaleDateString()}</p>
-                            <p>End Date: {new Date(tour.endDate).toLocaleDateString()}</p>
-                            <button onClick={() => joinTour(tour.tourId, tour.tourName)}>Tham gia</button>
+                    <li key={tour.tourId} style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', borderBottom: '1px solid #d9d9d9',}}>
+                        <div className='d-flex' style={{
+                            flex: '3'
+                        }}><img src={tour.tourImage} alt={tour.tourName} width="136" height={92} style={{ marginRight: '20px', borderRadius: '10px' }} />
+                            <div>
+                                <strong>{tour.tourName}</strong>
+                                <p className='m-0'>Location: {tour.location}</p>
+                                <p>{tour.numberOfDays} ngày {tour.numberOfNights} đêm </p>
+                            </div>
                         </div>
+
+                        <div style={{
+                            flex: '1'
+                        }} className='d-flex align-items-center fw-medium text-success'><p>{formatCurrency(tour.price)}</p></div>
+                        <div style={{
+                            flex: '1'
+                        }} className='d-flex align-items-center flex-column'>
+                            <p className='m-0'>5/{tour.maxGuests}</p>
+                            <small className='fw-medium'>{new Date(tour.startDate).toLocaleDateString()}</small>
+                            <small className='fw-medium'>{new Date(tour.endDate).toLocaleDateString()}</small>
+                        </div>
+                        <button className='rounded-5 border-0 text-light fw-normal' style={{
+                            height: '44px',
+                            width: '176px',
+                            backgroundColor: '#34A853',
+                        }} onClick={() => joinTour(tour.tourId, tour.tourName)}>Đặt chỗ ngay</button>
                     </li>
                 ))}
             </ul>
