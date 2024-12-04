df06331e440a0942791c7ad2da60cd006c5ecd9e
Nhơn Trần <nhonvn2509@gmail.com>
2024-12-04 16:15:32 +0700

    Add useEffect to handle login modal for private routes in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { publishRoutes, privateRoutes } from "./routes/AppRoute";
-import { Fragment } from "react"; 
+import { Fragment, useEffect } from "react"; // Added useEffect import
 import DefaultLayout from "./layouts/DefaultLayout";
 import { ToastContainer } from "react-toastify";
-import { useDispatch, useSelector } from "react-redux"; 
-import { openLoginModal } from "./redux/actions/modalActions"; 
-import { QueryClient, QueryClientProvider } from 'react-query'; // Added import
+import { useDispatch, useSelector } from "react-redux";
+import { openLoginModal } from "./redux/actions/modalActions";
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-const queryClient = new QueryClient(); // Added query client initialization
+const queryClient = new QueryClient();
 
 const RouteWrapper = ({ component: Component, layout: Layout, path }) => {
-  const dispatch = useDispatch(); 
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated); 
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  useEffect(() => {
+    if (privateRoutes.some(route => route.path === path) && !isAuthenticated) {
+      dispatch(openLoginModal());
+    }
+  }, [dispatch, isAuthenticated, path]);
 
   if (privateRoutes.some(route => route.path === path) && !isAuthenticated) {
-    
-    dispatch(openLoginModal());
     return <Navigate to="/" replace />;
-  } 
+  }
 
   if (Layout === null) {
     return (
@@ -32,8 +36,8 @@ const RouteWrapper = ({ component: Component, layout: Layout, path }) => {
   return (
     <AppliedLayout>
       <ToastContainer
-        position="bottom-right" 
-        autoClose={3000} 
+        position="bottom-right"
+        autoClose={3000}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick
@@ -50,7 +54,7 @@ const RouteWrapper = ({ component: Component, layout: Layout, path }) => {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router >
+      <Router>
         <Routes>
           {publishRoutes.map(({ path, component: Component, layout: Layout }, index) => (
             <Route
