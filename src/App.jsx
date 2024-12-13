53fd1fdbd832c417d79337ef24eb77a169134707
Nhơn Trần <nhonvn2509@gmail.com>
2024-12-13 16:45:56 +0700

    feat: enhance RouteWrapper with user role checks and toast notifications for unauthorized access

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,46 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate, useNavigate } from "react-router-dom";
 import { publishRoutes, privateRoutes } from "./routes/AppRoute";
-import { Fragment } from "react"; 
+import { Fragment, useEffect } from "react"; // Added useEffect import
 import DefaultLayout from "./layouts/DefaultLayout";
-import { ToastContainer } from "react-toastify";
-import { useDispatch, useSelector } from "react-redux"; 
-import { openLoginModal } from "./redux/actions/modalActions"; 
-import { QueryClient, QueryClientProvider } from 'react-query'; // Added import
+import { ToastContainer, toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import { openLoginModal } from "./redux/actions/modalActions";
+import { QueryClient, QueryClientProvider } from 'react-query';
+import RoutePath from "./routes/RoutePath";
 
-const queryClient = new QueryClient(); // Added query client initialization
+const queryClient = new QueryClient();
 
 const RouteWrapper = ({ component: Component, layout: Layout, path }) => {
-  const dispatch = useDispatch(); 
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated); 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const userRole = useSelector((state) => state.auth.user?.role);
+  console.log(userRole);
+  
+
+  useEffect(() => {
+    if (privateRoutes.some(route => route.path === path) && !isAuthenticated) {
+      toast.error("Bạn không có quyền truy cập trang này. Vui lòng đăng nhập để tiếp tục.");
+      navigate(RoutePath.AUTH);
+    }
+    // if(userRole === 'user' && (path === RoutePath.ADMIN || path === RoutePath.ADMIN_ACCOUNT_LIST || path === RoutePath.ADMIN_DESTINATION_MANAGEMENT || path === RoutePath.ADMIN_REPORT || path === RoutePath.ADMIN_TRANSACTION || path === RoutePath.ADMIN_TRIP_HISTORY)) {
+    //   navigate(RoutePath.HOMEPAGE);
+    //   toast.error("Bạn không có quyền truy cập trang này.");
+    // }
+    // if(userRole === 'admin' && (path !== RoutePath.ADMIN && path !== RoutePath.ADMIN_ACCOUNT_LIST && path !== RoutePath.ADMIN_DESTINATION_MANAGEMENT && path !== RoutePath.ADMIN_REPORT && path !== RoutePath.ADMIN_TRANSACTION && path !== RoutePath.ADMIN_TRIP_HISTORY && path !== RoutePath.AUTH)) {
+    //   navigate(RoutePath.ADMIN);
+    //   toast.error("Bạn không có quyền truy cập trang này. Vui lòng đăng nhập để tiếp tục");
+    // } 
+    // if(userRole === 'local' && (path !== RoutePath.LOCAL_CALENDAR_MANAGEMENT && path !== RoutePath.LOCAL_PLAN_MANAGEMENT && path !== RoutePath.LOCAL_STATICTIS && path !== RoutePath.LOCAL_TRIP_HISTORY && path !== RoutePath.LOCAL_WALLET_MANAGEMENT && path !== RoutePath.AUTH)) {
+    //   navigate(RoutePath.LOCAL);
+    //   toast.error("Bạn không có quyền truy cập trang này. Vui lòng đăng nhập để tiếp tục");
+    // }
+   
+  }, [dispatch, isAuthenticated, path, userRole]);
 
   if (privateRoutes.some(route => route.path === path) && !isAuthenticated) {
-    
-    dispatch(openLoginModal());
     return <Navigate to="/" replace />;
-  } 
+  }
 
   if (Layout === null) {
     return (
@@ -31,17 +54,6 @@ const RouteWrapper = ({ component: Component, layout: Layout, path }) => {
 
   return (
     <AppliedLayout>
-      <ToastContainer
-        position="bottom-right" 
-        autoClose={3000} 
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
       <Component />
     </AppliedLayout>
   );
@@ -50,7 +62,18 @@ const RouteWrapper = ({ component: Component, layout: Layout, path }) => {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router >
+      <Router>
+        <ToastContainer
+          position="bottom-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
         <Routes>
           {publishRoutes.map(({ path, component: Component, layout: Layout }, index) => (
             <Route
