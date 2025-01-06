a8e0f9d12289f3db153fa4b1c1f70048aa879c93
trannhon2509 <nhonvn2509@gmail.com>
2025-01-07 02:56:38 +0700

    Add edit functionality and modal to TourCard component

--- src/components/ProfileManagement/CreateTour.jsx
+++ src/components/ProfileManagement/CreateTour.jsx
@@ -2,9 +2,12 @@ import { useState, useEffect } from 'react';
 import Modal from 'react-modal';
 import '../../assets/css/ProfileManagement/CreateTour.css';
 import { createTour } from '../../apis/local_trip_history';
-import { Tabs, Tab } from 'react-bootstrap';
+import { Tabs, Tab, Accordion, Spinner } from 'react-bootstrap';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import TextareaAutosize from 'react-textarea-autosize';
+import { ref, uploadString, getDownloadURL } from 'firebase/storage';
+import { storage } from '../../../firebaseConfig';
 
 function CreateTour() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -20,6 +23,7 @@ function CreateTour() {
     schedules: [],
     additionalInfo: ""
   });
+  const [isCreating, setIsCreating] = useState(false);
 
   useEffect(() => {
     const totalActivityCost = tourData.itinerary.reduce((total, day) => {
@@ -74,8 +78,69 @@ function CreateTour() {
     setTourData({ ...tourData, schedules: [...tourData.schedules, { startDate: "2025-01-10T08:00:00Z", endDate: "2025-01-10T08:00:00Z" }] });
   };
 
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setTourData({ ...tourData, tourImage: reader.result });
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleActivityFileChange = (e, dayIndex, activityIndex) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const newItinerary = [...tourData.itinerary];
+        newItinerary[dayIndex].activities[activityIndex].activityImage = reader.result;
+        setTourData({ ...tourData, itinerary: newItinerary });
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   const handleCreateTour = async () => {
-    await createTour(tourData);
+    setIsCreating(true);
+    const uploadImage = async (imageData, path) => {
+      const storageRef = ref(storage, path);
+      await uploadString(storageRef, imageData, 'data_url');
+      return await getDownloadURL(storageRef);
+    };
+
+    const tourImageUrl = await uploadImage(tourData.tourImage, `tours/${tourData.tourName}/tourImage`);
+    const itinerary = await Promise.all(tourData.itinerary.map(async (day, dayIndex) => {
+      const activities = await Promise.all(day.activities.map(async (activity, activityIndex) => {
+        if (activity.activityImage) {
+          const activityImageUrl = await uploadImage(activity.activityImage, `tours/${tourData.tourName}/day${dayIndex + 1}/activity${activityIndex + 1}`);
+          return { ...activity, activityImage: activityImageUrl };
+        }
+        return activity;
+      }));
+      return { ...day, activities };
+    }));
+
+    const updatedTourData = { ...tourData, tourImage: tourImageUrl, itinerary };
+    await createTour(updatedTourData);
+    setIsCreating(false);
+  };
+
+  const handleDeleteItinerary = (dayIndex) => {
+    const newItinerary = tourData.itinerary.filter((_, index) => index !== dayIndex);
+    setTourData({ ...tourData, itinerary: newItinerary });
+  };
+
+  const handleDeleteActivity = (dayIndex, activityIndex) => {
+    const newItinerary = [...tourData.itinerary];
+    newItinerary[dayIndex].activities = newItinerary[dayIndex].activities.filter((_, index) => index !== activityIndex);
+    setTourData({ ...tourData, itinerary: newItinerary });
+  };
+
+  const handleDeleteCostDetail = (costIndex) => {
+    const newCostDetails = tourData.costDetails.filter((_, index) => index !== costIndex);
+    setTourData({ ...tourData, costDetails: newCostDetails });
   };
 
   return (
@@ -122,18 +187,17 @@ function CreateTour() {
               <label>Số khách tối đa</label>
               <input type="number" name="maxGuests" placeholder="Số khách tối đa" value={tourData.maxGuests} onChange={handleInputChange} />
               <label>Mô tả tour</label>
-              <textarea name="tourDescription" placeholder="Mô tả tour" value={tourData.tourDescription} onChange={handleInputChange}></textarea>
+              <TextareaAutosize minRows={3} name="tourDescription" placeholder="Mô tả tour" value={tourData.tourDescription} onChange={handleInputChange} />
             </div>
             <div className='col col-6 d-flex flex-column flex-grow-1 img'>
               <label>Hình ảnh tour</label>
-              <input type="text" name="tourImage" placeholder="Hình ảnh tour" value={tourData.tourImage} onChange={handleInputChange} />
-              <img src={tourData.tourImage} alt="ảnh tour" className='w-100'/>
+              <input type="file" accept="image/*" onChange={handleFileChange} />
+              <img src={tourData.tourImage} alt="ảnh tour" className='w-100' />
               <div>
-                Giá: {tourData.price}
+                Giá: {tourData.price.toLocaleString()} ₫
               </div>
             </div>
           </div>
-
           <div>
             <h5 className='text-uppercase fw-bold'>Lịch trình diễn ra</h5>
             <button onClick={handleAddSchedule}>Thêm lịch trình</button>
@@ -156,59 +220,90 @@ function CreateTour() {
           <Tabs defaultActiveKey="itinerary" id="uncontrolled-tab-example" className="mb-3 no-border-radius">
             <Tab eventKey="itinerary" title="Lịch trình cụ thể">
               <div>
-                <button onClick={handleAddItinerary}>Thêm lịch trình</button>
-                {tourData.itinerary.map((day, dayIndex) => (
-                  <div key={dayIndex}>
-                    <h3>Ngày {dayIndex + 1}</h3>
-                    <button onClick={() => handleAddActivity(dayIndex)}>Thêm hoạt động</button>
-                    {day.activities.map((activity, activityIndex) => (
-                      <div key={activityIndex}>
-                        <input type="text" placeholder="Tiêu đề hoạt động" value={activity.title} onChange={(e) => {
-                          const newItinerary = [...tourData.itinerary];
-                          newItinerary[dayIndex].activities[activityIndex].title = e.target.value;
-                          setTourData({ ...tourData, itinerary: newItinerary });
-                        }} />
-                        <input type="text" placeholder="Mô tả hoạt động" value={activity.description} onChange={(e) => {
-                          const newItinerary = [...tourData.itinerary];
-                          newItinerary[dayIndex].activities[activityIndex].description = e.target.value;
-                          setTourData({ ...tourData, itinerary: newItinerary });
-                        }} />
-                        <input type="text" placeholder="Địa chỉ hoạt động" value={activity.activityAddress} onChange={(e) => {
-                          const newItinerary = [...tourData.itinerary];
-                          newItinerary[dayIndex].activities[activityIndex].activityAddress = e.target.value;
-                          setTourData({ ...tourData, itinerary: newItinerary });
-                        }} />
-                        <input type="number" placeholder="Chi phí hoạt động" value={activity.activityAmount} onChange={(e) => {
-                          const newItinerary = [...tourData.itinerary];
-                          newItinerary[dayIndex].activities[activityIndex].activityAmount = parseInt(e.target.value);
-                          setTourData({ ...tourData, itinerary: newItinerary });
-                        }} />
-                        <input type="text" placeholder="Hình ảnh hoạt động" value={activity.activityImage} onChange={(e) => {
-                          const newItinerary = [...tourData.itinerary];
-                          newItinerary[dayIndex].activities[activityIndex].activityImage = e.target.value;
-                          setTourData({ ...tourData, itinerary: newItinerary });
-                        }} />
-                        <input type="time" placeholder="Thời gian bắt đầu" value={activity.startTime} onChange={(e) => {
-                          const newItinerary = [...tourData.itinerary];
-                          newItinerary[dayIndex].activities[activityIndex].startTime = e.target.value + ":00";
-                          setTourData({ ...tourData, itinerary: newItinerary });
-                        }} />
-                        <input type="time" placeholder="Thời gian kết thúc" value={activity.endTime} onChange={(e) => {
-                          const newItinerary = [...tourData.itinerary];
-                          newItinerary[dayIndex].activities[activityIndex].endTime = e.target.value + ":00";
-                          setTourData({ ...tourData, itinerary: newItinerary });
-                        }} />
-                      </div>
-                    ))}
-                  </div>
-                ))}
+                <button onClick={handleAddItinerary}>Thêm lịch trình cụ thể</button>
+                <Accordion defaultActiveKey="0">
+                  {tourData.itinerary.map((day, dayIndex) => (
+                    <Accordion.Item eventKey={dayIndex.toString()} key={dayIndex}>
+                      <Accordion.Header>
+                        Ngày {dayIndex + 1}
+                        <button style={{ marginLeft: '10px' }} onClick={() => handleDeleteItinerary(dayIndex)}>Xóa lịch trình cụ thể</button>
+                      </Accordion.Header>
+                      <Accordion.Body>
+                        <button onClick={() => handleAddActivity(dayIndex)}>Thêm hoạt động</button>
+                        {day.activities.map((activity, activityIndex) => (
+                          <div key={activityIndex} className='row'>
+                            <div className='col col-6 d-flex flex-column gap-2'>
+                              <label>Tiêu đề hoạt động</label>
+                              <input type="text" placeholder="Tiêu đề hoạt động" value={activity.title} onChange={(e) => {
+                                const newItinerary = [...tourData.itinerary];
+                                newItinerary[dayIndex].activities[activityIndex].title = e.target.value;
+                                setTourData({ ...tourData, itinerary: newItinerary });
+                              }} />
+
+                              <label>Địa chỉ hoạt động</label>
+                              <input type="text" placeholder="Địa chỉ hoạt động" value={activity.activityAddress} onChange={(e) => {
+                                const newItinerary = [...tourData.itinerary];
+                                newItinerary[dayIndex].activities[activityIndex].activityAddress = e.target.value;
+                                setTourData({ ...tourData, itinerary: newItinerary });
+                              }} />
+                              <div className='d-flex gap-2'>
+                                <div className='d-flex flex-column gap-2'>
+                                  <label>Thời gian bắt đầu</label>
+                                  <input type="time" placeholder="Thời gian bắt đầu" value={activity.startTime} onChange={(e) => {
+                                    const newItinerary = [...tourData.itinerary];
+                                    newItinerary[dayIndex].activities[activityIndex].startTime = e.target.value + ":00";
+                                    setTourData({ ...tourData, itinerary: newItinerary });
+                                  }} />
+                                </div>
+                                <div className='d-flex flex-column gap-2'>
+                                  <label>Thời gian kết thúc</label>
+                                  <input type="time" placeholder="Thời gian kết thúc" value={activity.endTime} onChange={(e) => {
+                                    const newItinerary = [...tourData.itinerary];
+                                    newItinerary[dayIndex].activities[activityIndex].endTime = e.target.value + ":00";
+                                    setTourData({ ...tourData, itinerary: newItinerary });
+                                  }} />
+                                </div>
+                                <div className='d-flex flex-column gap-2'>
+                                  <label>Chi phí hoạt động</label>
+                                  <input type="number" placeholder="Chi phí hoạt động" value={activity.activityAmount} onChange={(e) => {
+                                    const newItinerary = [...tourData.itinerary];
+                                    newItinerary[dayIndex].activities[activityIndex].activityAmount = parseInt(e.target.value);
+                                    setTourData({ ...tourData, itinerary: newItinerary });
+                                  }} />
+                                </div>
+                              </div>
+                              <label>Mô tả hoạt động</label>
+                              <TextareaAutosize minRows={2} placeholder="Mô tả hoạt động" value={activity.description} onChange={(e) => {
+                                const newItinerary = [...tourData.itinerary];
+                                newItinerary[dayIndex].activities[activityIndex].description = e.target.value;
+                                setTourData({ ...tourData, itinerary: newItinerary });
+                              }} />
+                            </div>
+                            <div className='col col-6 d-flex flex-column gap-2'>
+                              <label>Hình ảnh hoạt động</label>
+                              <input type="file" accept="image/*" onChange={(e) => handleActivityFileChange(e, dayIndex, activityIndex)} />
+                              {activity.activityImage && <img src={activity.activityImage} alt="ảnh hoạt động" className='w-100' />}
+                            </div>
+                            <div className='my-3'>
+                              <button style={{
+                                width: 'fit-content',
+                              }} onClick={() => handleDeleteActivity(dayIndex, activityIndex)}>Xóa hoạt động</button>
+
+                            </div>
+                            <hr />
+                          </div>
+                        ))}
+                      </Accordion.Body>
+                    </Accordion.Item>
+                  ))}
+                </Accordion>
               </div>
             </Tab>
             <Tab eventKey="cost" title="Chi phí">
               <div>
-                <button onClick={handleAddCostDetail}>Thêm chi phí</button>
+                <button className='mb-3' onClick={handleAddCostDetail}>Thêm chi phí</button>
                 {tourData.costDetails.map((cost, costIndex) => (
-                  <div key={costIndex}>
+                  <div key={costIndex} className='mb-3 d-flex gap-2'>
                     <input type="text" placeholder="Tiêu đề chi phí" value={cost.title} onChange={(e) => {
                       const newCostDetails = [...tourData.costDetails];
                       newCostDetails[costIndex].title = e.target.value;
@@ -224,6 +319,7 @@ function CreateTour() {
                       newCostDetails[costIndex].notes = e.target.value;
                       setTourData({ ...tourData, costDetails: newCostDetails });
                     }} />
+                    <button onClick={() => handleDeleteCostDetail(costIndex)}>Xóa chi phí</button>
                   </div>
                 ))}
               </div>
@@ -243,7 +339,10 @@ function CreateTour() {
           height: '50px',
         }}>
           <button className='btn btn-secondary' onClick={closeModal}>Đóng</button>
-          <button className='btn btn-success' onClick={handleCreateTour}>Tạo tour</button>
+          <button className='btn btn-success' onClick={handleCreateTour} disabled={isCreating}>
+            {isCreating && <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" />}
+            {isCreating ? ' Đang tạo...' : 'Tạo tour'}
+          </button>
         </div>
       </Modal>
     </div>
--- src/components/ProfileManagement/TourCard.jsx
+++ src/components/ProfileManagement/TourCard.jsx
@@ -2,26 +2,44 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../../assets/css/Tour/TourCard.css";
 import Dropdown from "react-bootstrap/Dropdown";
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
 import { deleteTour } from "../../apis/local_trip_history";
+import Modal from "react-modal"; // Import react-modal
+
+Modal.setAppElement('#root'); // Set the app element for accessibility
 
 function TourCard({ tour }) {
+    const [showModal, setShowModal] = useState(false);
+
     const handleDelete = async () => {
         if (window.confirm("Bạn có chắc chắn muốn xóa tour này không?")) {
             await deleteTour(tour.tourId);
         }
     };
 
+    const handleEdit = () => {
+        setShowModal(true);
+    };
+
+    const handleClose = () => {
+        setShowModal(false);
+    };
+
     return (
         <div className="card mb-3 tour-card">
             <div className="row g-0">
-                <div className="col-md-4">
+                <div className="col-md-2">
                     <img
                         src={tour.tourImage}
                         alt={tour.tourName}
                         className="img-fluid rounded-start"
+                        style={{
+                            maxHeight: "200px",
+                        }}
                     />
                 </div>
-                <div className="col-md-8 d-flex align-items-center">
+                <div className="col-md-10 d-flex align-items-center">
                     <div className="card-body flex-grow-1">
                         <h5 className="card-title">{tour.tourName}</h5>
                         <p className="card-text">{tour.tourDescription}</p>
@@ -45,7 +63,7 @@ function TourCard({ tour }) {
                             </Dropdown.Toggle>
                             <Dropdown.Menu>
                                 <Dropdown.Item >Xem chi tiết</Dropdown.Item>
-                                <Dropdown.Item >Chỉnh sửa</Dropdown.Item>
+                                <Dropdown.Item onClick={handleEdit}>Chỉnh sửa</Dropdown.Item>
                                 <Dropdown.Item >Quản lý</Dropdown.Item>
                                 <Dropdown.Item onClick={handleDelete}>Xóa</Dropdown.Item>
                             </Dropdown.Menu>
@@ -53,6 +71,35 @@ function TourCard({ tour }) {
                     </div>
                 </div>
             </div>
+            <Modal
+                isOpen={showModal}
+                onRequestClose={handleClose}
+                contentLabel="Chỉnh sửa tour"
+            >
+                <h2 style={{
+                    height: "50px",
+                }}>Chỉnh sửa tour</h2>
+                <div style={{
+                    height: "calc(100% - 100px)",
+                    overflowY: "auto",
+                }}>
+                    {/* Form chỉnh sửa tour */}
+                    
+                </div>
+                <div style={{
+                    height: "50px",
+                    display: "flex",
+                    justifyContent: "end",
+                    alignItems: "center",
+                }}>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Đóng
+                    </Button>
+                    <Button variant="primary">
+                        Lưu thay đổi
+                    </Button>
+                </div>
+            </Modal>
         </div>
     );
 }
